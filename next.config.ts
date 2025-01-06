6f6146c4adf68e16bc3003e1e81b3d26dc5706fc
NabiaHaider2 <nabiahaider11@gmail.com>
2025-01-06 15:25:13 +0500

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -8,7 +8,10 @@ const nextConfig: NextConfig = {
         hostname: 'cdn.sanity.io'
       }
     ]
-  }
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
   /* config options here */
 };
 
